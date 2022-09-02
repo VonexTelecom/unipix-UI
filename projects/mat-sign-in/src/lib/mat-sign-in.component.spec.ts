40b8744d05fdb82bb1576b9eb5f461fa0cacf5c3
shalomfernando <shalonando@gmail.com>
2022-09-02 15:26:02 -0300

    config

---
+++ projects/mat-sign-in/src/lib/mat-sign-in.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MatSignInComponent } from './mat-sign-in.component';
+
+describe('MatSignInComponent', () => {
+  let component: MatSignInComponent;
+  let fixture: ComponentFixture<MatSignInComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MatSignInComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MatSignInComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
